e49b18338f388f6ca6862b1ea771f1e05cb46044
gaozhenye <gaozhenye@embed-tec.com>
2022-11-28 14:51:22 +0800

    /'

--- assets/js/personal.js
+++ assets/js/personal.js
@@ -2,6 +2,7 @@ jQuery(function() {
   // 隐藏顶部和底部
   $(".site-header-nav").attr("style", "display:none;");
   $(".site-footer").attr("style", "display:none;");
-  
+  $(".collapsed").attr("style", "display:none;");
+   
   $(".collection-info").attr("style", "display:none;");
 });
